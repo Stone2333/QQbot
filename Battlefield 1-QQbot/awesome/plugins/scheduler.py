5e87733ebad1b06386ebaba6f1413af02628ba4b
Stone233 <45417764+Stone2333@users.noreply.github.com>
2019-11-12 18:34:11 +0800

    修复更新数据的BUG

--- Battlefield 1-QQbot/awesome/plugins/scheduler.py
+++ Battlefield 1-QQbot/awesome/plugins/scheduler.py
@@ -6,6 +6,7 @@ from aiocqhttp.exceptions import Error as CQHttpError
 import time
 import Mysql_Select
 import data_Mysql_Update
+import data_Mysql_Insert
 
 # 定时器
 @nonebot.scheduler.scheduled_job('cron', hour='*')
@@ -34,17 +35,17 @@ async def _():
         for index in range(len(All_User)):
             user_name = All_User[index]
             try:
-                retry_running(user_name, 1)
+                retry_running(user_name, 1, 1)
             except:
-                print(user_name, '战绩定时任务失败')
+                retry_running(user_name, 2, 1)
             try:
-                retry_running(user_name, 2)
+                retry_running(user_name, 1, 2)
             except:
-                print(user_name, '武器定时任务失败')
+                retry_running(user_name, 2, 2)
             try:
-                retry_running(user_name, 3)
+                retry_running(user_name, 1, 3)
             except:
-                print(user_name, '载具定时任务失败')
+                retry_running(user_name, 3, 3)
         End_Time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         print("结束时间：", End_Time)
         await bot.send_group_msg(group_id=552546607,
@@ -53,42 +54,84 @@ async def _():
         pass
 
 # 失败重跑
-def retry_running(user_name,type):
-    retry_times = 0
-    retry_count = 4
-    if type == 1:
-        while retry_times < retry_count:
-            try:
-                data_Mysql_Update.get_Update_Overview(user_name)
-            except:
-                retry_times = retry_times + 1
-                if retry_times == retry_count:
-                    print("武器重跑失败："+retry_times + user_name)
+def retry_running(user_name,type1,type2):
+    if type1 == 1:
+        retry_times = 0
+        retry_count = 4
+        if type2 == 1:
+            while retry_times < retry_count:
+                try:
+                    data_Mysql_Update.get_Update_Overview(user_name)
+                except:
+                    retry_times = retry_times + 1
+                    if retry_times == retry_count:
+                        print("战绩重跑失败：" + retry_times + user_name)
+                        retry_times == 0
+                else:
                     retry_times == 0
-            else:
-                retry_times == 0
-                break
-    elif type == 2:
-        while retry_times < retry_count:
-            try:
-                data_Mysql_Update.get_Update_Weapons(user_name)
-            except:
-                retry_times = retry_times + 1
-                if retry_times == retry_count:
-                    print("武器重跑失败：" + retry_times + user_name)
+                    break
+        elif type2 == 2:
+            while retry_times < retry_count:
+                try:
+                    data_Mysql_Update.get_Update_Weapons(user_name)
+                except:
+                    retry_times = retry_times + 1
+                    if retry_times == retry_count:
+                        print("武器重跑失败：" + retry_times + user_name)
+                        retry_times == 0
+                else:
                     retry_times == 0
-            else:
-                retry_times == 0
-                break
-    elif type == 3:
-        while retry_times < retry_count:
-            try:
-                data_Mysql_Update.get_Update_Vehicles(user_name)
-            except:
-                retry_times = retry_times + 1
-                if retry_times == retry_count:
-                    print("载具重跑失败："+retry_times + user_name)
+                    break
+        elif type2 == 3:
+            while retry_times < retry_count:
+                try:
+                    data_Mysql_Update.get_Update_Vehicles(user_name)
+                except:
+                    retry_times = retry_times + 1
+                    if retry_times == retry_count:
+                        print("载具重跑失败：" + retry_times + user_name)
+                        retry_times == 0
+                else:
                     retry_times == 0
-            else:
-                retry_times == 0
-                break
+                    break
+    elif type1 == 2:
+        retry_times = 0
+        retry_count = 4
+        if type2 == 1:
+            while retry_times < retry_count:
+                try:
+                    data_Mysql_Insert.get_Overview(user_name)
+                except:
+                    retry_times = retry_times + 1
+                    if retry_times == retry_count:
+                        print("战绩重跑失败：" + retry_times + user_name)
+                        retry_times == 0
+                else:
+                    retry_times == 0
+                    break
+        elif type2 == 2:
+            while retry_times < retry_count:
+                try:
+                    data_Mysql_Insert.get_Weapons(user_name)
+                except:
+                    retry_times = retry_times + 1
+                    if retry_times == retry_count:
+                        print("武器重跑失败：" + retry_times + user_name)
+                        retry_times == 0
+                else:
+                    retry_times == 0
+                    break
+        elif type2 == 3:
+            while retry_times < retry_count:
+                try:
+                    data_Mysql_Insert.get_Vehicles(user_name)
+                except:
+                    retry_times = retry_times + 1
+                    if retry_times == retry_count:
+                        print("载具重跑失败：" + retry_times + user_name)
+                        retry_times == 0
+                else:
+                    retry_times == 0
+                    break
+
+
