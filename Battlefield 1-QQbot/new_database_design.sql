b1730b5f6687f4afc592c3d2448638fd0788b557
Stone233 <45417764+Stone2333@users.noreply.github.com>
2020-01-06 11:50:08 +0800

    战地1数据库新表结构设计优化

--- Battlefield 1-QQbot/new_database_design.sql
+++ Battlefield 1-QQbot/new_database_design.sql
@@ -1,65 +1,77 @@
 CREATE TABLE `user` (
-`id` int(255) NOT NULL AUTO_INCREMENT,
-`name` char(30) NOT NULL,
-`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (`id`, `name`) 
+`id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+`name` char(20) NOT NULL COMMENT '游戏ID',
+`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '插入/更新时间',
+PRIMARY KEY (`id`, `name`)
 );
+
+
 CREATE TABLE `weapons` (
-`name` varchar(50) NOT NULL,
-`weapons_name` varchar(30) NOT NULL,
-`weapons_kills` varchar(30) NULL,
-`weapons_kpm` varchar(30) NULL,
-`weapons_accuracy` varchar(30) NULL,
-`weapons_headshots` varchar(30) NULL,
-`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (`name`, `weapons_name`) 
+`name` char(20) NOT NULL COMMENT '游戏ID',
+`weapons_name` char(20) NOT NULL COMMENT '武器名称',
+`weapons_kills` char(10) NULL COMMENT '武器击杀数',
+`weapons_kpm` char(5) NULL COMMENT '武器每分钟击杀数',
+`weapons_accuracy` char(5) NULL COMMENT '武器准度',
+`weapons_headshots` char(7) NULL COMMENT '武器爆头数',
+`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '插入/更新时间',
+PRIMARY KEY (`name`, `weapons_name`)
 );
+
+
 CREATE TABLE `recent_sesions` (
-`id` int(255) NOT NULL AUTO_INCREMENT,
-`name` varchar(50) NOT NULL,
-`spm` varchar(30) NULL,
-`kd` varchar(30) NULL,
-`kpm` varchar(30) NULL,
-`timeplayed` varchar(30) NULL,
-`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (`id`, `name`) 
+`id` int(10) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+`name` char(20) NOT NULL COMMENT '游戏ID',
+`spm` char(10) NULL COMMENT '每分钟得分数',
+`kd` char(10) NULL COMMENT '击杀/死亡比',
+`kpm` char(10) NULL COMMENT '每分钟击杀数 ',
+`timeplayed` char(15) NULL COMMENT '游戏时间',
+`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '插入/更新时间',
+PRIMARY KEY (`id`, `name`)
 );
+
+
 CREATE TABLE `vehicles` (
-`name` varchar(50) NOT NULL,
-`vehicles_name` varchar(50) NOT NULL,
-`vehicles_kills` varchar(30) NULL,
-`vehicles_kpm` varchar(30) NULL,
-`vehicles_destroyed` varchar(30) NULL,
-`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (`name`, `vehicles_name`) 
+`name` char(20) NOT NULL COMMENT '游戏ID',
+`vehicles_name` char(20) NOT NULL COMMENT '载具名称',
+`vehicles_kills` char(10) NULL COMMENT '载具击杀数',
+`vehicles_kpm` char(5) NULL COMMENT '载具每分钟击杀数',
+`vehicles_destroyed` char(7) NULL COMMENT '击毁载具数量',
+`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '插入/更新时间',
+PRIMARY KEY (`name`, `vehicles_name`)
 );
+
+
 CREATE TABLE `overview` (
-`name` varchar(50) NOT NULL,
-`score_min` varchar(30) NULL,
-`kd_ratio` varchar(30) NULL,
-`win_percent` varchar(30) NULL,
-`kills_game` varchar(30) NULL,
-`kills_min` varchar(30) NULL,
-`infantry_kpm` varchar(30) NULL,
-`infantry_kd` varchar(30) NULL,
-`vehicle_kills` varchar(30) NULL,
-`vehicle_kpm` varchar(30) NULL,
-`skill` varchar(30) NULL,
-`accuracy` varchar(30) NULL,
-`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (`name`) 
+`name` char(50) NOT NULL COMMENT '游戏ID',
+`score_min` char(5) NULL COMMENT '每分钟得分数',
+`kd_ratio` char(5) NULL COMMENT '击杀/死亡比',
+`win_percent` char(5) NULL COMMENT '胜率',
+`kills_game` char(5) NULL COMMENT '场均击杀',
+`kills_min` char(5) NULL COMMENT '每分钟击杀数',
+`infantry_kpm` char(5) NULL COMMENT '步兵每分钟击杀数',
+`infantry_kd` char(5) NULL COMMENT '步兵击杀/死亡比',
+`vehicle_kills` char(7) NULL COMMENT '载具击杀数',
+`vehicle_kpm` char(5) NULL COMMENT '载具每分钟击杀数',
+`skill` char(5) NULL COMMENT '技巧值',
+`accuracy` char(5) NULL COMMENT '准度',
+`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '插入/更新时间',
+PRIMARY KEY (`name`)
 );
+
+
 CREATE TABLE `server_id` (
-`server_id` int(30) NOT NULL,
-`server_as_name` varchar(100) NOT NULL,
-`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (`server_id`, `server_as_name`) 
+`server_id` int(30) NOT NULL COMMENT '服务器ID',
+`server_as_name` varchar(10) NOT NULL COMMENT '服务器别名',
+`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '插入/更新时间',
+PRIMARY KEY (`server_id`, `server_as_name`)
 );
+
+
 CREATE TABLE `server` (
-`server_id` int(30) NOT NULL,
-`server_name` varchar(50) NULL,
-`maplist` varchar(30) NULL,
-`prayers` varchar(30) NULL,
-`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (`server_id`) 
+`server_id` int(15) NOT NULL COMMENT '服务器ID',
+`server_name` char(70) NULL COMMENT '服务器名称',
+`maplist` char(10) NULL COMMENT '地图',
+`prayers` char(6) NULL COMMENT '游戏人数',
+`update_time` timestamp NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '插入/更新时间',
+PRIMARY KEY (`server_id`)
 );
