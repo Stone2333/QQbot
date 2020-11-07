455d82b35f9676e17a8074bcc79aca7dc603aa3e
Stone <374717284@qq.com>
2020-11-07 15:48:43 +0800

    正则

--- awesome/plugins/kuaicha2/__init__.py
+++ awesome/plugins/kuaicha2/__init__.py
@@ -2,10 +2,8 @@ from nonebot import on_command, CommandSession
 from nonebot import on_natural_language, NLPSession, IntentCommand
 
 
-
-
 @on_command('kuaicha2', patterns=[r'快速链接'], only_to_me=False)
-async def kuaicha2(session: CommandSession):
+async def kuaicha2(session:CommandSession):
     msg = """
     https://www.ceve-market.org/index/
     """
