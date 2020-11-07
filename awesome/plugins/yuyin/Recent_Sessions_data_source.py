717dd1ac8fa6bfe797e132f348c4cd158417ea5a
Stone <374717284@qq.com>
2020-11-07 22:12:26 +0800

    语音测试

--- awesome/plugins/yuyin/Recent_Sessions_data_source.py
+++ awesome/plugins/yuyin/Recent_Sessions_data_source.py
@@ -3,5 +3,5 @@ import re
 
 
 async def get_Recent_Sessions(Quer_Recent_Sessions: str) -> str:
-    s = '[CQ:record,file=file:///C:\\sy_audio.mp3]'
+    s = r'[CQ:record,file=file:///C:/sy_audio.mp3]'
     return s
