9cb9a56a525ab7ba0c5f542ea048636b33f795dc
Stone <374717284@qq.com>
2020-11-07 16:19:20 +0800

    去除空格

--- awesome/plugins/test/__init__.py
+++ awesome/plugins/test/__init__.py
@@ -12,5 +12,22 @@ __plugin_usage__ = r"""
 # 当用户输入关键字没有输入值时则提示
 @on_command('test', patterns=('色图','吃瓜','劳动仲裁'), only_to_me=False)
 async def test(session: CommandSession):
-    img = await get_img(str(session))
+    test1 = session.get('test1', prompt='你想注册ID是多少？')
+    img = await get_img(test1)
     await session.send(img)
+
+
+#  当用户输入关键字和值时直接运行
+@test.args_parser
+async def _(session: CommandSession):
+    stripped_arg = session.current_arg_text.strip()
+
+    if session.is_first_run:
+        if stripped_arg:
+            session.state['test1'] = stripped_arg
+        return
+
+    if not stripped_arg:
+        session.pause('要注册的ID不能为空，请重新输入')
+
+    session.state[session.current_key] = stripped_arg
