517c3445137b656dc974e4345e4ac3a1b57c2be1
Stone <374717284@qq.com>
2020-11-07 16:09:12 +0800

    去除

--- awesome/plugins/test/Login_data_source.py
+++ awesome/plugins/test/Login_data_source.py
@@ -9,7 +9,7 @@ async def get_img(Query_Login: str) -> str:
     elif Query_Login == '吃瓜':
         return '吃瓜'
     elif Query_Login == '快速链接':
-        s = """https://www.ceve-market.org/index/"""
+        s = "https://www.ceve-market.org/index/"
         return s
     elif Query_Login == '劳动仲裁':
         s = "文档：劳动仲裁和劳动法记录.note\n链接：http://note.youdao.com/noteshare?id=13ef9e3ff1cbcd4cb61b459c3e46cabf&sub=8C73DE805D394DF9A6C58AF74D900D06"
