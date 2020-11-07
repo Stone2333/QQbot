acebbffc88c393f3fcf1ff726ca8225060eba18b
Stone <374717284@qq.com>
2020-11-07 15:34:55 +0800

    正则

--- awesome/plugins/kuaicha2/__init__.py
+++ awesome/plugins/kuaicha2/__init__.py
@@ -1,14 +1,14 @@
 from nonebot import on_command, CommandSession
 from nonebot import on_natural_language, NLPSession, IntentCommand
-from .kuaicha import *
+
 
 
 
 
 # 当用户输入关键字没有输入值时则提示
-@on_command('kuaicha2', patterns=('快速链接'), only_to_me=False)
-async def kuaicha(session: CommandSession):
+@on_command('kuaicha2', patterns='快速链接', only_to_me=False)
+async def kuaicha2(session: CommandSession):
     msg = """
     https://www.ceve-market.org/index/
     """
-    await session.send(msg, at_sender=True)
+    await session.send(msg)
