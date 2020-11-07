06ed8da1c74c6a2a5eb07dddca60ebcc9a14423e
Stone <374717284@qq.com>
2020-11-07 15:33:00 +0800

    正则

---
+++ awesome/plugins/kuaicha2/__init__.py
@@ -0,0 +1,14 @@
+from nonebot import on_command, CommandSession
+from nonebot import on_natural_language, NLPSession, IntentCommand
+from .kuaicha import *
+
+
+
+
+# 当用户输入关键字没有输入值时则提示
+@on_command('kuaicha2', patterns=('快速链接'), only_to_me=False)
+async def kuaicha(session: CommandSession):
+    msg = """
+    https://www.ceve-market.org/index/
+    """
+    await session.send(msg, at_sender=True)
