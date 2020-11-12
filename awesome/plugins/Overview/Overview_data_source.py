5dae83d92adde690df47cb61d7a747f4dae06edd
Stone <374717284@qq.com>
2020-11-12 22:32:58 +0800

    修改战绩模板

--- awesome/plugins/Overview/Overview_data_source.py
+++ awesome/plugins/Overview/Overview_data_source.py
@@ -34,15 +34,15 @@ async def Select_Overview(Query_Overview: str) -> str:
             error = 'ID错误/橘子信息设置为隐私,无法查询到数据'
             return error
     else:
-        SCORE_MIN = "分数/分钟:" + Overview[0]
-        KD_RATIO = "K/D比:" + Overview[1]
+        SCORE_MIN = "每分钟得分:" + Overview[0]
+        KD_RATIO = "击毙死亡比:" + Overview[1]
         WIN_PERCENT = "胜率:" + Overview[2]
-        KILLS_GAME = "场均击杀:" + Overview[3]
-        KILLS_MIN = "杀敌/分钟:" + Overview[4]
-        INFANTRY_KPM = "步兵KPM:" + Overview[5]
-        INFANTRY_KD = "步兵KD:" + Overview[6]
-        VEHICLE_KILLS = "载具击杀:" + Overview[7]
-        VEHICLE_KPM = "载具KPM:" + Overview[8]
+        KILLS_GAME = "场均击毙:" + Overview[3]
+        KILLS_MIN = "每分钟击毙:" + Overview[4]
+        INFANTRY_KPM = "步兵每分钟击毙:" + Overview[5]
+        INFANTRY_KD = "步兵击毙/死亡比:" + Overview[6]
+        VEHICLE_KILLS = "载具击毙:" + Overview[7]
+        VEHICLE_KPM = "载具每分钟击毙:" + Overview[8]
         SKILL = "技巧值:" + Overview[9]
         ACCURACY = "准度:" + Overview[10]
         Overview_list = ["\n游戏ID:" + Query_Overview, SCORE_MIN, KD_RATIO, WIN_PERCENT, KILLS_GAME, KILLS_MIN, INFANTRY_KPM,INFANTRY_KD,VEHICLE_KILLS, VEHICLE_KPM, SKILL, ACCURACY]
