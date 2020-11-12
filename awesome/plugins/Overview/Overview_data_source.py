20adc441a9ae06dc2ada1405592937db8e44d548
Stone <374717284@qq.com>
2020-11-12 22:16:47 +0800

    修改战绩模板

--- awesome/plugins/Overview/Overview_data_source.py
+++ awesome/plugins/Overview/Overview_data_source.py
@@ -11,15 +11,15 @@ async def Select_Overview(Query_Overview: str) -> str:
             data_Mysql_Insert.get_Overview(Query_Overview)
             # 爬取完毕在数据库中查询
             Overview = Mysql_Select.Select_Overview(Query_Overview)
-            SCORE_MIN = "分数/分钟:" + Overview[0]
-            KD_RATIO = "K/D比:" + Overview[1]
+            SCORE_MIN = "每分钟得分:" + Overview[0]
+            KD_RATIO = "击毙死亡比:" + Overview[1]
             WIN_PERCENT = "胜率:" + Overview[2]
-            KILLS_GAME = "场均击杀:" + Overview[3]
-            KILLS_MIN = "杀敌/分钟:" + Overview[4]
-            INFANTRY_KPM = "步兵KPM:" + Overview[5]
-            INFANTRY_KD = "步兵KD:" + Overview[6]
-            VEHICLE_KILLS = "载具击杀:" + Overview[7]
-            VEHICLE_KPM = "载具KPM:" + Overview[8]
+            KILLS_GAME = "场均击毙:" + Overview[3]
+            KILLS_MIN = "每分钟击毙:" + Overview[4]
+            INFANTRY_KPM = "步兵每分钟击毙:" + Overview[5]
+            INFANTRY_KD = "步兵击毙/死亡比:" + Overview[6]
+            VEHICLE_KILLS = "载具击毙:" + Overview[7]
+            VEHICLE_KPM = "载具每分钟击毙:" + Overview[8]
             SKILL = "技巧值:" + Overview[9]
             ACCURACY = "准度:" + Overview[10]
             Overview_list = ["\n游戏ID:" + Query_Overview, SCORE_MIN, KD_RATIO, WIN_PERCENT, KILLS_GAME, KILLS_MIN,
