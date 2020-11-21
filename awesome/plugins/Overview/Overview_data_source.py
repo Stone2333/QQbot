9c12a8c13281597bdea33b975cfe73720e4be17d
Stone <374717284@qq.com>
2020-11-21 12:22:39 +0800

    战绩

--- awesome/plugins/Overview/Overview_data_source.py
+++ awesome/plugins/Overview/Overview_data_source.py
@@ -10,22 +10,18 @@ async def Select_Overview(Query_Overview: str) -> str:
         try:
             data_Mysql_Insert.get_Overview(Query_Overview)
             # 爬取完毕在数据库中查询
-            Overview = Mysql_Select.Select_Overview(Query_Overview)
-            SCORE_MIN = "每分钟得分:" + Overview[0]
-            KD_RATIO = "KD:" + Overview[1]
+            SCORE_MIN = "每分钟得多少分:" + Overview[0]
+            KD_RATIO = "击毙去世比:" + Overview[1]
             WIN_PERCENT = "胜率:" + Overview[2]
-            KILLS_GAME = "场均击毙:" + Overview[3]
-            KILLS_MIN = "KPM:" + Overview[4]
-            INFANTRY_KPM = "步兵KPM:" + Overview[5]
-            INFANTRY_KD = "步兵KD:" + Overview[6]
+            KILLS_GAME = "每场击毙:" + Overview[3]
+            KILLS_MIN = "每分钟干多少:" + Overview[4]
+            INFANTRY_KPM = "步兵每分钟干多少:" + Overview[5]
+            INFANTRY_KD = "步兵击毙去世比:" + Overview[6]
             VEHICLE_KILLS = "载具击毙:" + Overview[7]
-            VEHICLE_KPM = "载具KPM:" + Overview[8]
+            VEHICLE_KPM = "载具击毙去世比:" + Overview[8]
             ACCURACY = "准度:" + Overview[10]
-            Overview_list = ["\n游戏ID:" + Query_Overview, SCORE_MIN, KD_RATIO, WIN_PERCENT, KILLS_GAME, KILLS_MIN,
-                             INFANTRY_KPM,
-                             INFANTRY_KD,
-                             VEHICLE_KILLS, VEHICLE_KPM, ACCURACY]
-            # 去逗号
+            Overview_list = ["\n游戏名称:" + Query_Overview, SCORE_MIN, KD_RATIO, WIN_PERCENT, KILLS_GAME, KILLS_MIN,
+                             INFANTRY_KPM, INFANTRY_KD, VEHICLE_KILLS, VEHICLE_KPM, ACCURACY]
             Overview_str = (' \n'.join(Overview_list))
             print('这是爬虫爬取完成后查到的战绩数据')
             return Overview_str
@@ -34,17 +30,17 @@ async def Select_Overview(Query_Overview: str) -> str:
             return error
     else:
 
-        SCORE_MIN = "每分钟得分:" + Overview[0]
-        KD_RATIO = "KD:" + Overview[1]
+        SCORE_MIN = "每分钟得多少分:" + Overview[0]
+        KD_RATIO = "击毙去世比:" + Overview[1]
         WIN_PERCENT = "胜率:" + Overview[2]
-        KILLS_GAME = "场均击毙:" + Overview[3]
-        KILLS_MIN = "KPM:" + Overview[4]
-        INFANTRY_KPM = "步兵KPM:" + Overview[5]
-        INFANTRY_KD = "步兵KD:" + Overview[6]
+        KILLS_GAME = "每场击毙:" + Overview[3]
+        KILLS_MIN = "每分钟干多少:" + Overview[4]
+        INFANTRY_KPM = "步兵每分钟干多少:" + Overview[5]
+        INFANTRY_KD = "步兵击毙去世比:" + Overview[6]
         VEHICLE_KILLS = "载具击毙:" + Overview[7]
-        VEHICLE_KPM = "载具KPM:" + Overview[8]
+        VEHICLE_KPM = "载具击毙去世比:" + Overview[8]
         ACCURACY = "准度:" + Overview[10]
-        Overview_list = ["\n游戏ID:" + Query_Overview, SCORE_MIN, KD_RATIO, WIN_PERCENT, KILLS_GAME, KILLS_MIN, INFANTRY_KPM,INFANTRY_KD,VEHICLE_KILLS, VEHICLE_KPM, ACCURACY]
+        Overview_list = ["\n游戏名称:" + Query_Overview, SCORE_MIN, KD_RATIO, WIN_PERCENT, KILLS_GAME, KILLS_MIN, INFANTRY_KPM,INFANTRY_KD,VEHICLE_KILLS, VEHICLE_KPM, ACCURACY]
         Overview_str = (' \n'.join(Overview_list))
         print('这是直接查数据库查到的战绩数据')
         return Overview_str
