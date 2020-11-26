35b15b050e70003f6cabaf6f10e49754c75b7f24
Stone <374717284@qq.com>
2020-11-26 14:37:01 +0800

    快速查询修改

--- awesome/plugins/test/Login_data_source.py
+++ awesome/plugins/test/Login_data_source.py
@@ -5,6 +5,9 @@ import delete
 import re
 import data_Mysql_Insert
 import data_Mysql_Update
+import Mysql_Insert
+import json
+import Mysql_Update
 
 async def get_img(qq, Query_Login: str, session) -> str:
     relevance = Mysql_Select.Select_Id(qq)
@@ -70,54 +73,70 @@ async def get_img(qq, Query_Login: str, session) -> str:
         if relevance == ():
             return 'qq号暂未绑定游戏ID,请使用绑定关键字绑定游戏ID 绑定 XXX'
         else:
-            Overview = Mysql_Select.Select_Overview(relevance[0][0])
-            # 判断数据库中有没有这个ID，有则直接在数据库中查询，没有则调用爬虫爬取插入
-            if Overview == []:
-                # 调用爬虫爬取插入
-                try:
-                    data_Mysql_Insert.get_Overview(relevance[0][0])
-                    # 爬取完毕在数据库中查询
-                    Overview = Mysql_Select.Select_Overview(relevance[0][0])
-                    SCORE_MIN = "分数/分钟:" + Overview[0]
-                    KD_RATIO = "K/D比:" + Overview[1]
-                    WIN_PERCENT = "胜率:" + Overview[2]
-                    KILLS_GAME = "场均击杀:" + Overview[3]
-                    KILLS_MIN = "杀敌/分钟:" + Overview[4]
-                    INFANTRY_KPM = "步兵KPM:" + Overview[5]
-                    INFANTRY_KD = "步兵KD:" + Overview[6]
-                    VEHICLE_KILLS = "载具击杀:" + Overview[7]
-                    VEHICLE_KPM = "载具KPM:" + Overview[8]
-                    SKILL = "技巧值:" + Overview[9]
-                    ACCURACY = "准度:" + Overview[10]
-                    Overview_list = ["\n游戏ID:" + relevance[0][0], SCORE_MIN, KD_RATIO, WIN_PERCENT, KILLS_GAME,
-                                     KILLS_MIN,
-                                     INFANTRY_KPM,
-                                     INFANTRY_KD,
-                                     VEHICLE_KILLS, VEHICLE_KPM, SKILL, ACCURACY]
-                    # 去逗号
-                    Overview_str = (' \n'.join(Overview_list))
-                    print('这是爬虫爬取完成后查到的战绩数据')
-                    return Overview_str
-                except:
-                    error = 'ID错误/橘子信息设置为隐私,无法查询到数据'
-                    return error
+            prompt = "查询中请稍候"
+            await session.send(prompt)
+            name = Mysql_Select.get_overview_name(relevance[0][0])
+            msg = overview(relevance[0][0])
+            print(msg)
+            if msg == '我们找不到您的统计信息，请确保您名称正确':
+                if not name:
+                    return '\n' + msg + '\n' + '由于没有查询过战绩所以没有历史数据'
+                msg1 = get_db_overview(relevance[0][0])
+                return '\n' + msg + '\n' + '以下数据是历史数据仅供参考:' + '\n' + msg1
+            elif msg == '尝试更新统计信息时发生错误,简而言之就是网站挂了,具体啥时间恢复我也不知道':
+                if not name:
+                    return '\n' + msg + '\n' + '由于没有查询过战绩所以没有历史数据'
+                msg1 = get_db_overview(relevance[0][0])
+                return '\n' + msg + '\n' + '以下数据是历史数据仅供参考:' + '\n' + msg1
+            elif msg == '很抱歉,在执行您的要求时发生了一个错误,错误报告已提交给管理员,他们将立即修复该错误!,简而言之也是服务器挂了的一种,恢复时间俺也不知道':
+                if not name:
+                    return '\n' + msg + '\n' + '由于没有查询过战绩所以没有历史数据'
+                msg1 = get_db_overview(relevance[0][0])
+                return '\n' + msg + '\n' + '以下数据是历史数据仅供参考:' + '\n' + msg1
+            elif msg == '战绩网数据库维护,请稍后再试':
+                if not name:
+                    return '\n' + msg + '\n' + '由于没有查询过战绩所以没有历史数据'
+                msg1 = get_db_overview(relevance[0][0])
+                return '\n' + msg + '\n' + '以下数据是历史数据仅供参考:' + '\n' + msg1
+            elif msg == '网络问题,请稍后再试':
+                if not name:
+                    return '\n' + msg + '\n' + '由于没有查询过战绩所以没有历史数据'
+                msg1 = get_db_overview(relevance[0][0])
+                return '\n' + msg + '\n' + '以下数据是历史数据仅供参考:' + '\n' + msg1
+            if not name:
+                rank = msg[0]
+                win_percent = msg[1]
+                kd = msg[2]
+                kpm = msg[3]
+                all_kills = msg[4]
+                head_shots_odds = msg[5]
+                accuracy_ratio = msg[6]
+                infantry_kd = msg[7]
+                infantry_kpm = msg[8]
+                vehicle_kills = msg[9]
+                vehicle_kpm = msg[10]
+                Mysql_Insert.insert_overview_info(relevance[0][0], rank, win_percent, kd, kpm, all_kills,
+                                                  head_shots_odds, accuracy_ratio, infantry_kd, infantry_kpm,
+                                                  vehicle_kills, vehicle_kpm)
+                msg = get_db_overview(relevance[0][0])
+                return '\n' + msg
             else:
-                SCORE_MIN = "分数/分钟:" + Overview[0]
-                KD_RATIO = "K/D比:" + Overview[1]
-                WIN_PERCENT = "胜率:" + Overview[2]
-                KILLS_GAME = "场均击杀:" + Overview[3]
-                KILLS_MIN = "杀敌/分钟:" + Overview[4]
-                INFANTRY_KPM = "步兵KPM:" + Overview[5]
-                INFANTRY_KD = "步兵KD:" + Overview[6]
-                VEHICLE_KILLS = "载具击杀:" + Overview[7]
-                VEHICLE_KPM = "载具KPM:" + Overview[8]
-                SKILL = "技巧值:" + Overview[9]
-                ACCURACY = "准度:" + Overview[10]
-                Overview_list = ["\n游戏ID:" + relevance[0][0], SCORE_MIN, KD_RATIO, WIN_PERCENT, KILLS_GAME, KILLS_MIN,
-                                 INFANTRY_KPM, INFANTRY_KD, VEHICLE_KILLS, VEHICLE_KPM, SKILL, ACCURACY]
-                Overview_str = (' \n'.join(Overview_list))
-                print('这是直接查数据库查到的战绩数据')
-                return Overview_str
+                rank = msg[0]
+                win_percent = msg[1]
+                kd = msg[2]
+                kpm = msg[3]
+                all_kills = msg[4]
+                head_shots_odds = msg[5]
+                accuracy_ratio = msg[6]
+                infantry_kd = msg[7]
+                infantry_kpm = msg[8]
+                vehicle_kills = msg[9]
+                vehicle_kpm = msg[10]
+                Mysql_Update.update_overview_info(relevance[0][0], rank, win_percent, kd, kpm, all_kills,
+                                                  head_shots_odds, accuracy_ratio, infantry_kd, infantry_kpm,
+                                                  vehicle_kills, vehicle_kpm)
+                msg = get_db_overview(relevance[0][0])
+                return '\n' + msg
     elif Query_Login == '.武器':
         if relevance == ():
             return 'qq号暂未绑定游戏ID,请使用绑定关键字绑定游戏ID 绑定 XXX'
@@ -180,57 +199,54 @@ async def get_img(qq, Query_Login: str, session) -> str:
                 Weapons_str = (' \n'.join(Weapons_list))
                 print('这是直接查数据库查到的武器数据')
                 return Weapons_str
+
     elif Query_Login == '.载具':
         if relevance == ():
             return 'qq号暂未绑定游戏ID,请使用绑定关键字绑定游戏ID 绑定 XXX'
         else:
-            Vehicles = Mysql_Select.Select_Vehicles(relevance[0][0])
-            if Vehicles == []:
-                try:
-                    prompt = "查询中请稍候"
-                    await session.send(prompt)
-                    data_Mysql_Insert.get_Vehicles(relevance[0][0])
-                    Vehicles = Mysql_Select.Select_Vehicles(relevance[0][0])
-                    name = Vehicles[0]
-                    kills = Vehicles[1]
-                    kpm = Vehicles[2]
-                    Destroyed = Vehicles[3]
-                    name1 = Vehicles[4]
-                    kills1 = Vehicles[5]
-                    kpm1 = Vehicles[6]
-                    Destroyed1 = Vehicles[7]
-                    name2 = Vehicles[8]
-                    kills2 = Vehicles[9]
-                    kpm2 = Vehicles[10]
-                    Destroyed2 = Vehicles[11]
-                    Vehicles_list = ["\n载具名称:" + name, "击毙:" + kills, "每分钟得分:" + kpm, "击毁载具:" + Destroyed,
-                                     "载具名称:" + name1, "击毙:" + kills1, "每分钟得分:" + kpm1, "击毁载具:" + Destroyed1,
-                                     "载具名称:" + name2, "击毙:" + kills2, "每分钟得分:" + kpm2, "击毁载具:" + Destroyed2]
-                    Vehicles_str = (' \n'.join(Vehicles_list))
-                    print('这是爬虫爬取完成后查到的载具数据')
-                    return Vehicles_str
-                except:
-                    error = 'ID错误/橘子信息设置为隐私/没用过载具,无法查询到数据'
-                    return error
+            name = Mysql_Select.get_db_vehicles_all(relevance[0][0])
+            msg = vehicles(relevance[0][0])
+            if msg == '我们找不到您的统计信息，请确保您名称正确':
+                if not name:
+                    return '\n' + msg + '\n' + '由于没有查询过载具所以没有历史数据'
+                msg1 = get_db_vehicles(relevance[0][0])
+                return '\n' + msg + '\n' + '以下数据是历史数据仅供参考:' + '\n' + msg1
+            elif msg == '尝试更新统计信息时发生错误,简而言之就是网站挂了,具体啥时间恢复我也不知道':
+                if not name:
+                    return '\n' + msg + '\n' + '由于没有查询过载具所以没有历史数据'
+                msg1 = get_db_vehicles(relevance[0][0])
+                return '\n' + msg + '\n' + '以下数据是历史数据仅供参考:' + '\n' + msg1
+            elif msg == '很抱歉,在执行您的要求时发生了一个错误,错误报告已提交给管理员,他们将立即修复该错误!,简而言之也是服务器挂了的一种,恢复时间俺也不知道':
+                if not name:
+                    return '\n' + msg + '\n' + '由于没有查询过载具所以没有历史数据'
+                msg1 = get_db_vehicles(relevance[0][0])
+                return '\n' + msg + '\n' + '以下数据是历史数据仅供参考:' + '\n' + msg1
+            elif msg == '战绩网数据库维护,请稍后再试':
+                if not name:
+                    return '\n' + msg + '\n' + '由于没有查询过载具所以没有历史数据'
+                msg1 = get_db_vehicles(relevance[0][0])
+                return '\n' + msg + '\n' + '以下数据是历史数据仅供参考:' + '\n' + msg1
+            elif msg == '网络问题,请稍后再试':
+                if not name:
+                    return '\n' + msg + '\n' + '由于没有查询过载具所以没有历史数据'
+                msg1 = get_db_vehicles(relevance[0][0])
+                return '\n' + msg + '\n' + '以下数据是历史数据仅供参考:' + '\n' + msg1
+
+            if not name or len(name) < 3:
+                if len(name) == 0:
+                    insert_vehicles_data(relevance[0][0], msg)
+                    msg = get_db_vehicles(relevance[0][0])
+                    return '\n' + msg
+                else:
+                    delete.delete_vehicles(relevance[0][0])
+                    insert_vehicles_data(relevance[0][0], msg)
+                    msg = get_db_vehicles(relevance[0][0])
+                    return '\n' + msg
             else:
-                name = Vehicles[0]
-                kills = Vehicles[1]
-                kpm = Vehicles[2]
-                Destroyed = Vehicles[3]
-                name1 = Vehicles[4]
-                kills1 = Vehicles[5]
-                kpm1 = Vehicles[6]
-                Destroyed1 = Vehicles[7]
-                name2 = Vehicles[8]
-                kills2 = Vehicles[9]
-                kpm2 = Vehicles[10]
-                Destroyed2 = Vehicles[11]
-                Vehicles_list = ["\n载具名称:" + name, "击毙:" + kills, "每分钟得分:" + kpm, "击毁载具:" + Destroyed,
-                                 "载具名称:" + name1, "击毙:" + kills1, "每分钟得分:" + kpm1, "击毁载具:" + Destroyed1,
-                                 "载具名称:" + name2, "击毙:" + kills2, "每分钟得分:" + kpm2, "击毁载具:" + Destroyed2]
-                Vehicles_str = (' \n'.join(Vehicles_list))
-                print('这是直接查数据库查到的载具数据')
-                return Vehicles_str
+                update_vehicles_data(relevance[0][0], msg)
+                msg = get_db_vehicles(relevance[0][0])
+                return '\n' + msg
+
     elif Query_Login == '帮助':
         s = '[CQ:image,file=file:///C:\\1.png]'
         return s
@@ -280,6 +296,170 @@ def get_recent_sessions(Quer_Recent_Sessions):
     return game_play_time, spm, kd, kpm, game_time
 
 
+def overview(name):
+    """
+    获取服务器信息
+
+    :param name:
+    :return:
+    """
+    url_join = "https://battlefieldtracker.com/bf1/profile/pc/" + name
+    # url = base_url.format(url1)
+    try:
+        headers = {
+            "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+        }
+        # proxies = {
+        #     'http': 'username:password@125.123.122.178:9999',
+        # }
+        response = requests.get(url_join, headers=headers, timeout=15)
+        htmlContent = response.content.decode("utf-8")
+    except:
+        return '网络问题,请稍后再试'
+    msg1 = error(htmlContent)
+    msg2 = error2(htmlContent)
+    msg3 = error3(htmlContent)
+    msg4 = error4(htmlContent)
+    if msg1 == '我们找不到您的统计信息，请确保您名称正确':
+        return msg1
+    elif msg2 == '尝试更新统计信息时发生错误,简而言之就是网站挂了,具体啥时间恢复我也不知道':
+        return msg2
+    elif msg3 == '很抱歉,在执行您的要求时发生了一个错误,错误报告已提交给管理员,他们将立即修复该错误!,简而言之也是服务器挂了的一种,恢复时间俺也不知道':
+        return msg3
+    elif msg4 == '战绩网数据库维护,请稍后再试':
+        return msg4
+    string2 = htmlContent.replace('\n', '').replace('\r', '').replace(r' ', '')
+    # 等级
+    patt4 = '<spanclass="title">Rank(.*?)</span>'
+    rank = re.findall(pattern=patt4, string=string2)
+    # 总击杀
+    patt7 = 'data-stat="Kills">(.*?)</div>'
+    all_kills1 = re.findall(pattern=patt7, string=string2)
+    all_kills = all_kills1[0]
+    int_all_kills = int(all_kills1[0].replace(',', ''))
+    # 爆头击杀
+    patt8 = 'data-stat="HeadShots">(.*?)</div>'
+    head_shots_kills = re.findall(pattern=patt8, string=string2)
+    int_head_shots_kills = int(head_shots_kills[0].replace(',', ''))
+    # 爆头率
+    head_shots_odds = round(int_all_kills / int_head_shots_kills, 1)
+    patt6 = "varcurrentStatsJson='(.*?)';trnDeltas."
+    all = re.findall(pattern=patt6, string=string2)
+    all_list = json.loads(all[0])
+    all_dict = {}
+    for i in all_list:
+        name = i['Field']
+        number = i['Value']
+        all_dict[name] = number
+    kd = all_dict['Kd']
+    win_percent = all_dict['Wl']
+    kpm = all_dict['Kpm']
+    # 场均击杀
+    infantry_kpm = all_dict['InfantryKpm']
+    infantry_kd = all_dict['InfantryKd']
+    vehicle_kills = all_dict['VehicleKills']
+    vehicle_kpm = all_dict['VehicleKPM']
+    # 准度
+    accuracy_ratio = all_dict['AccuracyRatio']
+    return rank[0], win_percent, kd, kpm, all_kills, head_shots_odds, float(accuracy_ratio) * 100, infantry_kd, infantry_kpm, vehicle_kills, vehicle_kpm
+
+
+def vehicles(name):
+    try:
+        url = f"https://battlefieldtracker.com/bf1/profile/pc/{name}/vehicles"
+        headers = {
+            "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+        }
+        # proxies = {
+        #     'http': 'username:password@222.89.32.173:9999'
+        # }
+        response = requests.get(url, headers=headers, timeout=15)
+        html = response.content.decode("utf-8")
+    except:
+        return '网络问题,请稍后再试'
+    msg = error(html)
+    msg2 = error2(html)
+    msg3 = error3(html)
+    msg4 = error4(html)
+    if msg == '我们找不到您的统计信息，请确保您名称正确':
+        return msg
+    elif msg2 == '尝试更新统计信息时发生错误,简而言之就是网站挂了,具体啥时间恢复我也不知道':
+        return msg2
+    elif msg3 == '很抱歉,在执行您的要求时发生了一个错误,错误报告已提交给管理员,他们将立即修复该错误!,简而言之也是服务器挂了的一种,恢复时间俺也不知道':
+        return msg3
+    elif msg4 == '战绩网数据库维护,请稍后再试':
+        return msg4
+    string2 = html.replace('\n', '').replace('\r', '').replace(' ', '')
+    # 名字
+    patt = '<trclass=""><tdclass="details"><divclass="title">(.*?)<ahref="'
+    vehicles_name_list = re.findall(pattern=patt, string=string2)
+    # 载具击杀
+    patt2 = '</a></div></td><tdclass="statdetailed"><divclass="value">(.*?)</div><divclass="rank">'
+    vehicles_kills_all_list = re.findall(pattern=patt2, string=string2)
+    vehicles_kills_list = vehicles_kills_all_list[0:3]
+    # kpm
+    patt3 = '</div></div></div></div></td><tdclass="statdetailed"><divclass="value">(.*?)</div><divclass="rank">'
+    vehicles_kpm_all_list = re.findall(pattern=patt3, string=string2)
+    vehicles_kpm_list = vehicles_kpm_all_list[0:3]
+    # 击毁
+    patt4 = '"></div></div></div></div></td><tdclass="stat">(.*?)</td>'
+    vehicles_destroyed_all_list = re.findall(pattern=patt4, string=string2)
+    vehicles_destroyed_list = vehicles_destroyed_all_list[0:3]
+    # 时间
+    patt5 = '\d</td><tdclass="stat">(.*?)</td></tr>'
+    vehicles_time_all_list = re.findall(pattern=patt5, string=string2)
+    vehicles_time_list = vehicles_time_all_list[0:3]
+    return vehicles_name_list, vehicles_kills_list, vehicles_kpm_list, vehicles_destroyed_list, vehicles_time_list
+
+
+def get_db_vehicles(name):
+    msg = Mysql_Select.get_db_vehicles_all(name)
+    string2 = ""
+    for m in msg:
+        vehicles_name = m['vehicles_name']
+        vehicles_kills = m['vehicles_kills']
+        vehicles_kpm = m['vehicles_kpm']
+        vehicles_destroyed = m['vehicles_destroyed']
+        vehicles_time = m['vehicles_time']
+        string2 += \
+f"""载具名称:{vehicles_name}
+击毙:{vehicles_kills}
+KPM:{vehicles_kpm}
+击毁载具:{vehicles_destroyed}
+使用时间:{vehicles_time}
+===============
+"""
+    return string2
+
+
+def get_db_overview(name):
+    msg = Mysql_Select.get_db_overview_all(name)
+    string2 = ""
+    for m in msg:
+        rank = m['rank']
+        win_percent = m['win_percent']
+        kd = m['kd']
+        kpm = m['kpm']
+        all_kills = m['all_kills']
+        head_shots_odds = m['head_shots_odds']
+        accuracy_ratio = m['accuracy_ratio']
+        infantry_kd = m['infantry_kd']
+        infantry_kpm = m['infantry_kpm']
+        vehicle_kills = m['vehicle_kills']
+        vehicle_kpm = m['vehicle_kpm']
+        string2 += f"""等级:{rank}
+胜率:{win_percent}%
+kd:{kd}
+kpm:{kpm}
+总击毙:{all_kills}
+爆头率:{head_shots_odds}%
+准度:{accuracy_ratio}%
+步兵kd:{infantry_kd}
+步兵kpm:{infantry_kpm}
+载具击毙:{vehicle_kills}
+载具kpm:{vehicle_kpm}"""
+    return string2
+
 
 def get_db_recent_sessions(name):
     msg = Mysql_Select.get_recent_sessions_all(name)
@@ -310,6 +490,41 @@ def error(html):
         return "我们找不到您的统计信息，请确保您名称正确"
     else:
         pass
+
+def insert_vehicles_data(name, msg):
+    vehicles_name_list = msg[0]
+    vehicles_kills_list = msg[1]
+    vehicles_kpm_list = msg[2]
+    vehicles_destroyed_list = msg[3]
+    vehicles_time_list = msg[4]
+    for vehicles_name, vehicles_kills, vehicles_kpm_list, vehicles_destroyed, vehicles_time_list in zip(
+            vehicles_name_list, vehicles_kills_list, vehicles_kpm_list, vehicles_destroyed_list,
+            vehicles_time_list):
+        map_dict = {'ASSAULTTANK': '圣沙蒙', 'ASSAULTTRUCK': '菊花车', 'AttackPlane': '攻击机', 'Horse': '马',
+                    'Landship': '巡航坦克', 'HEAVYBOMBER': '重型轰炸机', 'LightTank': '轻型坦克', 'HeavyTank': '重型坦克',
+                     'StationaryWeapon': '固定武器', 'ArtilleryTruck': '火炮车', 'Behemoth': '巨兽', 'Bomber': '轰炸机'
+                      , 'Boat': '船', 'Fighter': '战斗机'}
+        Mysql_Insert.insert_vehicles_info(
+                name, map_dict[vehicles_name], vehicles_kills, vehicles_kpm_list, vehicles_destroyed, vehicles_time_list)
+
+
+def update_vehicles_data(name, msg):
+    id_tuple = Mysql_Select.get_vehicles_name(name)
+    vehicles_name_list = msg[0]
+    vehicles_kills_list = msg[1]
+    vehicles_kpm_list = msg[2]
+    vehicles_destroyed_list = msg[3]
+    vehicles_time_list = msg[4]
+    for id, vehicles_name, vehicles_kills, vehicles_kpm_list, vehicles_destroyed, vehicles_time_list in zip(
+            id_tuple, vehicles_name_list, vehicles_kills_list, vehicles_kpm_list, vehicles_destroyed_list,
+            vehicles_time_list):
+            map_dict = {'ASSAULTTANK': '圣沙蒙', 'ASSAULTTRUCK': '菊花车', 'AttackPlane': '攻击机', 'Horse': '马',
+                    'Landship': '巡航坦克', 'HEAVYBOMBER': '重型轰炸机', 'LightTank': '轻型坦克', 'HeavyTank': '重型坦克',
+                    'StationaryWeapon': '固定武器', 'ArtilleryTruck': '火炮车', 'Behemoth': '巨兽', 'Bomber': '轰炸机'
+            , 'Boat': '船', 'Fighter': '战斗机'}
+            Mysql_Update.update_vehicles_info(
+                name, id[0], map_dict[vehicles_name], vehicles_kills, vehicles_kpm_list, vehicles_destroyed, vehicles_time_list)
+
 
 
 def error2(html):
