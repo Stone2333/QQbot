0bdb2da6095dc1c90f24242652fb1535bfa81b05
石文兵 <374717284@qq.com>
2020-12-11 11:02:41 +0800

    修改最近战绩文案

--- awesome/plugins/new_recent_sessions/Recent_Sessions_data_source.py
+++ awesome/plugins/new_recent_sessions/Recent_Sessions_data_source.py
@@ -110,11 +110,11 @@ def get_db_recent_sessions(name):
         kpm = m['kpm']
         game_play_time = m['game_play_time']
         game_time = m['game_time']
-        string2 +=f"""玩耍日期:{game_play_time}
-每分钟干分:{spm}
-干和被干比:{kd}
-每分钟干多少个:{kpm}
-游玩时间:{game_time}
+        string2 +=f"""game_time:{game_play_time}
+spm:{spm}
+kd:{kd}
+kpm:{kpm}
+play_time:{game_time}
 *=============*
 """
     return string2
