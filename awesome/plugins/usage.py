f3a62d5eca8e401eb464b73273f1f28726c3e414
石文兵 <374717284@qq.com>
2021-01-28 13:48:56 +0800

    群号

--- awesome/plugins/usage.py
+++ awesome/plugins/usage.py
@@ -1,24 +1,24 @@
-import nonebot
-from nonebot import on_command, CommandSession
-
-
-@on_command('usage', aliases=['使用帮助', '帮助', '使用方法'], only_to_me=False)
-async def _(session: CommandSession):
-    # 获取设置了名称的插件列表
-    bot = nonebot.get_bot()
-    list = await bot.get_group_list()
-    print(list)
-    plugins = list(filter(lambda p: p.name, nonebot.get_loaded_plugins()))
-    arg = session.current_arg_text.strip().lower()
-    if not arg:
-        # 如果用户没有发送参数，则发送功能列表
-        await session.send(
-            '我现在支持的功能有：\n' + '\n'.join(p.name for p in plugins))
-        return
-
-    # 如果发了参数则发送相应命令的使用帮助
-    for p in plugins:
-        if p.name.lower() == arg:
-            await session.send(p.usage)
-
+# import nonebot
+# from nonebot import on_command, CommandSession
 #
+#
+# @on_command('usage', aliases=['使用帮助', '帮助', '使用方法'], only_to_me=False)
+# async def _(session: CommandSession):
+#     # 获取设置了名称的插件列表
+#     bot = nonebot.get_bot()
+#     list = await bot.get_group_list()
+#     print(list)
+#     plugins = list(filter(lambda p: p.name, nonebot.get_loaded_plugins()))
+#     arg = session.current_arg_text.strip().lower()
+#     if not arg:
+#         # 如果用户没有发送参数，则发送功能列表
+#         await session.send(
+#             '我现在支持的功能有：\n' + '\n'.join(p.name for p in plugins))
+#         return
+#
+#     # 如果发了参数则发送相应命令的使用帮助
+#     for p in plugins:
+#         if p.name.lower() == arg:
+#             await session.send(p.usage)
+#
+# #
